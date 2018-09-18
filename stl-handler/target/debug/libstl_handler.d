991b93da15dd8db9d04909ea82a07597d4d027e1
Unknown <touisbetterthanpi@gmail.com>
2018-09-17 23:34:19 -0500

    added pub methods for triangle fields

---
+++ stl-handler/target/debug/libstl_handler.d
@@ -0,0 +1,1 @@
+/Users/davidsullivan/Desktop/Programing/Rust/stl-handler/stl-handler/target/debug/libstl_handler.rlib: /Users/davidsullivan/Desktop/Programing/Rust/stl-handler/stl-handler/src/lib.rs
